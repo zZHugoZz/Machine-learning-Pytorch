7f04f7de209213f32eb9681542632589c45218f3
zZHugoZz <123295904+zZHugoZz@users.noreply.github.com>
2023-09-05 11:51:53 +0200

    added doctrings

--- early_stopping.py
+++ early_stopping.py
@@ -6,6 +6,16 @@ from torch.optim import Optimizer
 
 class EarlyStopping:
     def __init__(self, patience: int = 5, min_delta: float = 0.0) -> None:
+        """Class that implements early stopping
+
+        Args:
+            patience (int, optional): The number of steps to wait
+                without improvement before stopping the training. Defaults to 5.
+            min_delta (float, optional): Minimum change in the monitored
+                metric to qualify as an improvement.
+                If the improvement is less than `min_delta`,
+                it is not considered significant. Defaults to 0.0.
+        """
         self.patience = patience
         self.min_delta = min_delta
         self.best_model_state_dict = None
@@ -18,6 +28,13 @@ class EarlyStopping:
     def __call__(
         self, test_loss: float, model: nn.Module, optimizer: Optimizer
     ) -> None:
+        """Checks if early stopping criteria are met.
+
+        Args:
+            test_loss (float): The current test loss.
+            model (nn.Module): The model being trained.
+            optimizer (Optimizer): The optimizer used for training.
+        """
         if test_loss > (self.min_test_loss + self.min_delta):
             self.counter += 1
 
@@ -34,6 +51,16 @@ class EarlyStopping:
     def restore_best(
         self, model: nn.Module, optimizer: Optimizer
     ) -> tuple[dict, dict, Iterator]:
+        """Restores the best model and optimizer states.
+
+        Args:
+            model (nn.Module): The nmodel being trained.
+            optimizer (Optimizer): The optimizer used for training.
+
+        Returns:
+            tuple[dict, dict, Iterator]: A tuple containing the best model state dictionary,
+            best optimizer state dictionary, and best model parameters.
+        """
         if (
             self.best_model_state_dict is not None
             and self.best_optimizer_state_dict is not None
